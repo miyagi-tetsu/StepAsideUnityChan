a67aab0a91ce25e2fdd10b728ab1fc9f86a287f5
miyagi tetsu <otaji@koneko2006.com>
2018-02-20 23:12:48 +0900

    StepAsideUnityChan 課題提出
    Lesson6  課題、発展課題を行いました。

---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Lesson6課題：不可視化状態に読み込められる
+	public void OnBecameInvisible(){
+		Destroy(this.gameObject);
+
+	}
+
+}
