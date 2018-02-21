aa2f35f6c49356cf36e14c5eda49a30a15181d4e
miyagi tetsu <otaji@koneko2006.com>
2018-02-21 21:07:09 +0900

    StepAsideUnityChan 課題再提出
    Lesson6  課題、発展課題を行いました。

--- Assets/DestroyObject.cs
+++ Assets/DestroyObject.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class DestroyObject : MonoBehaviour {
 
+//Lesson6課題：不可視化状態に読み込められる
+	private GameObject unityChan;
+
+	private float  odj;            //オブジェクト用変数作成
+//Lesson6課題：不可視化状態に読み込められる
+
 	// Use this for initialization
 	void Start () {
 
+//Lesson6課題：不可視化状態に読み込められる
+		unityChan = GameObject.Find("unitychan");
 
+		odj = this.transform.position.z + 10;　　//アタッチしたオブジェクトのｚ+10を座標取得（通り過ぎてすぐ消えない様に+10）
+//Lesson6課題：不可視化状態に読み込められる
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
+//Lesson6課題：不可視化状態に読み込められる
+		if (odj < unityChan.transform.position.z) {　　　//unityChanがこのオブジェクトを通過したら動作
+			Destroy(this.gameObject);
+		}
+//Lesson6課題：不可視化状態に読み込められる
+
 	}
 
-	//Lesson6課題：不可視化状態に読み込められる
-	public void OnBecameInvisible(){
-		Destroy(this.gameObject);
-
-	}
+	//Lesson6課題：不可視化状態に読み込められる-再提出のため不採用(シーン全体表示だと消えないため)
+//	public void OnBecameInvisible(){
+//		Destroy(this.gameObject);
+//	}
 
 }
